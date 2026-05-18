f0a30b07ec3cf09f399cc2d7d7cc4e2c59b7c687
2iamQuan <quannvm@vnpt.vn>
2026-05-18 16:56:08 +0700

    feat: Enhance AppointmentListProcessor to retrieve patient and doctor profiles based on user role

--- src/main/java/com/camel/clinic/processor/appointment/AppointmentListProcessor.java
+++ src/main/java/com/camel/clinic/processor/appointment/AppointmentListProcessor.java
@@ -1,6 +1,13 @@
 package com.camel.clinic.processor.appointment;
 
+import com.camel.clinic.entity.PatientProfile;
+import com.camel.clinic.entity.Role;
+import com.camel.clinic.repository.DoctorProfileRepository;
+import com.camel.clinic.repository.PatientProfileRepository;
+import com.camel.clinic.service.CommonService;
 import com.camel.clinic.service.appointment.AppointmentServiceImp;
+import com.camel.clinic.util.JwtUtil;
+import com.camel.clinic.util.SecuritiesUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.Exchange;
@@ -15,10 +22,33 @@ import java.util.Map;
 @Slf4j
 public class AppointmentListProcessor implements Processor {
     private final AppointmentServiceImp serviceImp;
+    private final PatientProfileRepository patientProfileRepository;
+    private final DoctorProfileRepository doctorProfileRepository;
+
+    private final JwtUtil jwtUtil;
 
     @Override
     public void process(Exchange exchange) {
         Map<String, Object> queryParams = exchange.getIn().getHeaders();
+
+        Role.RoleName role = SecuritiesUtils.getRole();
+
+        String userId = jwtUtil.getUserIdFromToken(SecuritiesUtils.getAccessToken(exchange));
+        if (role == Role.RoleName.PATIENT) {
+            PatientProfile profile = patientProfileRepository.findByUserId(CommonService.parseToUuid(userId))
+                    .orElseThrow(() -> new RuntimeException(
+                            "Patient profile not found for user ID: " + userId));
+
+            queryParams.put("patientProfileId", profile.getId().toString());
+
+        }
+        if (role == Role.RoleName.DOCTOR) {
+            doctorProfileRepository.findByUserId(CommonService.parseToUuid(userId))
+                    .orElseThrow(() -> new RuntimeException(
+                            "Doctor profile not found for user ID: " + userId));
+
+            queryParams.put("doctorProfileId", userId);
+        }
 
         ResponseEntity<?> response = serviceImp.list(queryParams);
 
